73bb6428a6a91fa18a0b5ea31cdbfecb7becbc5b
epaule <mh6@sanger.ac.uk>
2023-01-26 14:09:16 +0000

    version 1

---
+++ README.md
@@ -0,0 +1,26 @@
+[![Built with Crystal](https://img.shields.io/badge/built%20with-crystal-000000.svg?style=flat-square)](https://crystal-lang.org/)
+[![CI](https://github.com/Aquatic-Symbiosis-Genomics-Project/grit-rename/actions/workflows/ci.yml/badge.svg)](https://github.com/Aquatic-Symbiosis-Genomics-Project/grit-rename/actions?query=workflow%3ACI)
+[![Latest Release](https://img.shields.io/github/v/release/Aquatic-Symbiosis-Genomics-Project/grit-rename.svg)](https://github.com/Aquatic-Symbiosis-Genomics-Project/grit-rename/releases)
+# grit-rename
+
+Tool to rename TPF/AGP/FASTA files based on a mapping
+
+## Usage
+
+TODO: Write usage instructions here
+
+## Development
+
+TODO: Write development instructions here
+
+## Contributing
+
+1. Fork it (<https://github.com/your-github-user/grit-rename/fork>)
+2. Create your feature branch (`git checkout -b my-new-feature`)
+3. Commit your changes (`git commit -am 'Add some feature'`)
+4. Push to the branch (`git push origin my-new-feature`)
+5. Create a new Pull Request
+
+## Contributors
+
+- [epaule](https://github.com/your-github-user) - creator and maintainer
---
+++ src/grit-rename.cr
@@ -0,0 +1,98 @@
+#!/usr/bin/env crystal
+# grit-rename -m TSV -f XYZ.fa -a XYZ.agp  -t XYZ.tpf
+
+# then there is the decision between standardout and an output file
+
+
+class Mapper
+	def initialize(file : String , outdir : String)
+		@mapping = Hash(String, String).new
+    @outdir = outdir
+		self.parse_mapping(file)
+    Dir.mkdir_p(outdir)
+	end
+
+	# parse the mapping
+	# mapping between scaffold_ids -> new_ids
+	def parse_mapping(file)
+		File.each_line(file) do |line|
+			key,val = line.split(/\s+/)
+			@mapping[key] = val
+		end
+	end
+
+	# add id mappings from the TPF
+	def munge_ids(file)
+		File.each_line(file) do |line|
+			c = line.split(/\s+/)
+
+			# might need a bit to deal with the id:from-to
+
+			@mapping[c[2]] = @mapping[c[1]] if @mapping.has_key?(c[1])
+		end
+	end
+
+	# edit the files
+
+	## fasta
+	def fasta(file)
+    o_file=File.new("#{@outdir}/#{File.basename(file)}.fa","w")
+
+		File.each_line(file) do |line|
+			if />(\w+)(_unloc\d+)*/.match(line)
+				line=line.sub($1,@mapping[$1]) if @mapping.has_key?($1)
+			end
+			o_file.puts line
+		end
+	end
+
+	## tpf
+	def tpf(file)
+    o_file=File.new("#{@outdir}/#{File.basename(file)}.tpf","w")
+		File.each_line(file) do |line|
+			c = line.split("\t")
+			if /(\w+)(_unloc\d+)*/.match(c[2])
+				c[2]=c[2].sub($1,@mapping[$1]) if @mapping.has_key?($1)
+				line = c.join("\t")
+			end
+			o_file.puts line
+		end
+	end
+
+	## agp
+	# split by tab then take the first column
+	# column[0](_unloc)*
+
+	## int
+	# ???
+end
+
+require "option_parser"
+
+mapping_file ="" # mapper
+fasta_file=""
+tpf_file=""
+# agp_file=""
+out_dir=""
+OptionParser.parse do |parser|
+  parser.banner = "Usage: grit-rename --map MAPPINGs --fasta FASTAFILE --outdir OUTDIR --tpf TPFFILE"
+  parser.on("-m MAPPINGs","--map=MAPPINGS","mappings TSV file FROM<tab>TO"){|m| mapping_file=m}
+  parser.on("-f FASTAFILE","--fasta=FASTAFILE","FASTA FILE to rename"){|f| fasta_file=f}
+  parser.on("-t TPFFILE","--tpf=TPFFILE","TPF FILE to RENAME"){|t|tpf_file=t}
+  parser.on("-o OUTDIR","--outdir=OUTDIR","directory for the renamed files"){|o|out_dir=o}
+  parser.on("-h","--help","Show this help") do
+    puts parser
+    exit
+  end
+
+  parser.invalid_option do |flag|
+    STDERR.puts "ERROR: #{flag} is not a valid option."
+    STDERR.puts parser
+    exit(1)
+  end
+end
+
+mapper=Mapper.new(mapping_file,out_dir)
+mapper.fasta(fasta_file) if fasta_file
+# m.munge_ids(ARGV[1])    #munging ?
+mapper.tpf(tpf_file) if tpf_file
